81efd0d4332b054297ee00549fbc5809e0788526
rheapradhan21 <87064604+rheapradhan21@users.noreply.github.com>
2023-09-29 16:11:46 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -13,4 +13,22 @@ recognition.onresult = function (event){
     console.log(event);
     var content = event.results[0][0].transcript;
     document.getElementById("textbox").innerHTML = content;
+    speak();
 }
+Webcam.set({
+    width:360,
+    height:250,
+    image_format : 'png',
+    png_quality:90
+});
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data = document.getElementById("textbox").value;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+
+    Webcam.attach("#camera");
+}
+
+
+
