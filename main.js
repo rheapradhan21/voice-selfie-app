d67bf0988ef00abc1a5e8e33c7a87e61a9c681de
rheapradhan21 <87064604+rheapradhan21@users.noreply.github.com>
2023-10-02 17:30:01 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -13,22 +13,44 @@ recognition.onresult = function (event){
     console.log(event);
     var content = event.results[0][0].transcript;
     document.getElementById("textbox").innerHTML = content;
-    speak();
+    if(content=="Take my selfie"){
+        console.log("Taking selfie");
+        speak();
+    }
+    
 }
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data = "Taking your selfie in 5 seconds";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+
+    Webcam.attach(camera);
+    setTimeout(function(){
+        take_selfie();
+        save();
+    },5000);
+}
+camera = document.getElementById("camera");
 Webcam.set({
     width:360,
     height:250,
     image_format : 'png',
     png_quality:90
 });
-function speak(){
-    var synth = window.speechSynthesis;
-    speak_data = document.getElementById("textbox").value;
-    var utterThis = new SpeechSynthesisUtterance(speak_data);
-    synth.speak(utterThis);
 
-    Webcam.attach("#camera");
+
+function take_selfie(){
+    Webcam.snap(function(data_uri){
+        document,getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
+    });
 }
 
+function save(){
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src;
+    link.href = image;
+    link.click();
+}
 
 
